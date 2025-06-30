e1c7b8c9cff0fbdfef02598d2badc2f63ad94fdc
John Mugambi Ndeke <mugambindeke@gmail.com>
2025-06-30 08:20:25 -0700

    Update serializers.py

--- backend/apps/payments/serializers.py
+++ backend/apps/payments/serializers.py
@@ -1,1 +1,8 @@
 
+from rest_framework import serializers
+from .models import Payment
+
+class PaymentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Payment
+        fields = ['id', 'user', 'project', 'amount', 'status', 'paystack_reference', 'created_at']
