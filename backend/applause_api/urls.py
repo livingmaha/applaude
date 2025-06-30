1a2e2591e8efffd9922b0bb8a3092f2e5a237bcd
John Mugambi Ndeke <mugambindeke@gmail.com>
2025-06-30 08:22:52 -0700

    Update urls.py

--- backend/applause_api/urls.py
+++ backend/applause_api/urls.py
@@ -5,6 +5,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/users/', include('apps.users.urls')),
-    # Future paths for other apps will go here
-    # path('api/projects/', include('apps.projects.urls')),
+    path('api/projects/', include('apps.projects.urls')),
+    path('api/payments/', include('apps.payments.urls')),
 ]
