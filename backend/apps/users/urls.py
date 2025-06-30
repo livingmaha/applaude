34420f5815fb22dc562ba67b2a9eab1ff9c465c7
John Mugambi Ndeke <mugambindeke@gmail.com>
2025-06-30 07:02:15 -0700

    Update urls.py

--- backend/apps/users/urls.py
+++ backend/apps/users/urls.py
@@ -1,1 +1,10 @@
+# File: /backend/apps/users/urls.py
+from django.urls import path
+from .views import CreateUserView, CreateTokenView
 
+app_name = 'users'
+
+urlpatterns = [
+    path('create/', CreateUserView.as_view(), name='create'),
+    path('token/', CreateTokenView.as_view(), name='token'),
+]
