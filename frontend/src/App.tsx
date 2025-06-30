2a1c9ddeb306fa4909c5c2d4fc0fd2dd8d5258d5
John Mugambi Ndeke <mugambindeke@gmail.com>
2025-06-30 06:58:09 -0700

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,1 +1,12 @@
+import React from 'react';
+import Onboarding from './pages/Onboarding';
 
+function App() {
+  return (
+    <div className="bg-quantum-black min-h-screen">
+      <Onboarding />
+    </div>
+  );
+}
+
+export default App;
