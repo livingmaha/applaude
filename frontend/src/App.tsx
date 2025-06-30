94fbd20aa3a372db364274cc05b2f50a1cf03613
John Mugambi Ndeke <mugambindeke@gmail.com>
2025-06-30 07:34:46 -0700

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,24 @@
+// File: /frontend/src/App.tsx
 import React from 'react';
-import Onboarding from './pages/Onboarding';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import SignUpPage from './pages/SignUpPage';
+import Onboarding from './pages/Onboarding'; // We'll use this later
+// We will create LoginPage and Dashboard soon
+// import LoginPage from './pages/LoginPage';
+// import Dashboard from './pages/Dashboard';
 
 function App() {
   return (
-    <div className="bg-quantum-black min-h-screen">
-      <Onboarding />
-    </div>
+    <Router>
+      <Routes>
+        {/* For now, we'll make SignUp the main page. Later, this will be a landing page. */}
+        <Route path="/" element={<SignUpPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        {/* <Route path="/login" element={<LoginPage />} /> */}
+        {/* <Route path="/onboarding" element={<Onboarding />} /> */}
+        {/* <Route path="/dashboard" element={<Dashboard />} /> */}
+      </Routes>
+    </Router>
   );
 }
 
