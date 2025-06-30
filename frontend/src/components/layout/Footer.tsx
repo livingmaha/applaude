1dd7973d5faf5fc0787f6657fafacfa4cafaf1e0
John Mugambi Ndeke <mugambindeke@gmail.com>
2025-06-30 08:42:32 -0700

    Update Footer.tsx

--- frontend/src/components/layout/Footer.tsx
+++ frontend/src/components/layout/Footer.tsx
@@ -1,1 +1,13 @@
 
+import React from 'react';
+
+const Footer = () => {
+    const currentYear = new Date().getFullYear();
+    return (
+        <footer className="w-full bg-black bg-opacity-20 mt-16 p-6 text-center text-gray-400">
+            <p>&copy; 2025 Applause Inc. All rights reserved.</p>
+        </footer>
+    );
+};
+
+export default Footer;
