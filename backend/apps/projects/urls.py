a09947e4350d7c07fe219c3c1b95871987f3423d
John Mugambi Ndeke <mugambindeke@gmail.com>
2025-06-30 08:05:05 -0700

    Update urls.py

--- backend/apps/projects/urls.py
+++ backend/apps/projects/urls.py
@@ -1,1 +1,13 @@
 
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import ProjectViewSet
+
+# Create a router and register our viewsets with it.
+router = DefaultRouter()
+router.register(r'', ProjectViewSet, basename='project')
+
+# The API URLs are now determined automatically by the router.
+urlpatterns = [
+    path('', include(router.urls)),
+]
