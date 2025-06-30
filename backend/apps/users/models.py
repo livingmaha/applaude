61b401a4fdfc99ab9cfd3f9bab19e324a1be7ca4
John Mugambi Ndeke <mugambindeke@gmail.com>
2025-06-30 07:01:28 -0700

    Update models.py

--- backend/apps/users/models.py
+++ backend/apps/users/models.py
@@ -1,1 +1,9 @@
+# File: /backend/apps/users/models.py
+from django.contrib.auth.models import AbstractUser
+from django.db import models
 
+class CustomUser(AbstractUser):
+    # We can add new fields here in the future
+    # For example: profile_picture = models.ImageField(upload_to='profile_pics/', null=True, blank=True)
+    def __str__(self):
+        return self.username
