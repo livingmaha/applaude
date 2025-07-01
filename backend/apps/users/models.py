05fab9544b572806dd40c493de2f81bc9855a115
John Mugambi Ndeke <mugambindeke@gmail.com>
2025-07-01 05:26:43 -0700

    Update models.py

--- backend/apps/users/models.py
+++ backend/apps/users/models.py
@@ -1,9 +1,8 @@
-# File: /backend/apps/users/models.py
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 class CustomUser(AbstractUser):
-    # We can add new fields here in the future
-    # For example: profile_picture = models.ImageField(upload_to='profile_pics/', null=True, blank=True)
+    is_premium_subscribed = models.BooleanField(default=False)
+    
     def __str__(self):
         return self.username
