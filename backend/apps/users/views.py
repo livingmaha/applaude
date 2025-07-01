d85c0066e3912e2eed08a71e7ba093f859026318
John Mugambi Ndeke <mugambindeke@gmail.com>
2025-07-01 06:01:19 -0700

    Update views.py

--- backend/apps/users/views.py
+++ backend/apps/users/views.py
@@ -1,25 +1,33 @@
-# File: /backend/apps/users/views.py
-from rest_framework import generics, permissions
+
+from rest_framework import generics, permissions, status
+from rest_framework.views import APIView
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.authtoken.models import Token
 from rest_framework.response import Response
-from .serializers import UserSerializer
+from .serializers import UserSerializer, CustomUserDetailSerializer
+from .models import CustomUser
 
 class CreateUserView(generics.CreateAPIView):
-    """Create a new user in the system"""
     serializer_class = UserSerializer
-    permission_classes = [permissions.AllowAny] # Anyone can create a user
+    permission_classes = [permissions.AllowAny]
 
 class CreateTokenView(ObtainAuthToken):
-    """Create a new auth token for user"""
     def post(self, request, *args, **kwargs):
-        serializer = self.serializer_class(data=request.data,
-                                           context={'request': request})
+        serializer = self.serializer_class(data=request.data, context={'request': request})
         serializer.is_valid(raise_exception=True)
         user = serializer.validated_data['user']
         token, created = Token.objects.get_or_create(user=user)
         return Response({
             'token': token.key,
             'user_id': user.pk,
-            'email': user.email
+            'email': user.email,
+            'is_premium_subscribed': user.is_premium_subscribed
         })
+
+class ManageUserView(generics.RetrieveUpdateAPIView):
+    """Manage the authenticated user"""
+    serializer_class = CustomUserDetailSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_object(self):
+        return self.request.user
