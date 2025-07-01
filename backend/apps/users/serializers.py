8d843ac41ba184d0b26b4d0ae3596c088a81990e
John Mugambi Ndeke <mugambindeke@gmail.com>
2025-07-01 06:01:47 -0700

    Update serializers.py

--- backend/apps/users/serializers.py
+++ backend/apps/users/serializers.py
@@ -1,4 +1,4 @@
-# File: /backend/apps/users/serializers.py
+
 from django.contrib.auth import get_user_model
 from rest_framework import serializers
 
@@ -6,12 +6,17 @@ User = get_user_model()
 
 class UserSerializer(serializers.ModelSerializer):
     """Serializer for the user object"""
-
     class Meta:
         model = User
         fields = ('id', 'email', 'username', 'password')
         extra_kwargs = {'password': {'write_only': True, 'min_length': 8}}
 
     def create(self, validated_data):
-        """Create a new user with an encrypted password and return it"""
         return User.objects.create_user(**validated_data)
+
+class CustomUserDetailSerializer(serializers.ModelSerializer):
+    """Serializer for the detailed user view"""
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'email', 'is_premium_subscribed')
+        read_only_fields = ('id', 'email', 'is_premium_subscribed')
