ccc858beaf1bcbd3e431d000a585a70fef4f3e01
John Mugambi Ndeke <mugambindeke@gmail.com>
2025-07-01 05:31:06 -0700

    Create urls.py

---
+++ backend/apps/surveys/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from .views import (
+    SubmitSurveyResponseView, 
+    SubmitAppRatingView, 
+    SubmitUserFeedbackView,
+    ProjectAnalyticsView
+)
+
+urlpatterns = [
+    path('submit/survey/', SubmitSurveyResponseView.as_view(), name='submit-survey'),
+    path('submit/rating/', SubmitAppRatingView.as_view(), name='submit-rating'),
+    path('submit/feedback/', SubmitUserFeedbackView.as_view(), name='submit-feedback'),
+    path('analytics/<int:pk>/', ProjectAnalyticsView.as_view(), name='project-analytics'),
+]
