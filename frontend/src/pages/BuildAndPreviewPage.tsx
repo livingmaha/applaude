9d66a890ee924499dae5e1fdc3f71a6d11dd8d3f
John Mugambi Ndeke <mugambindeke@gmail.com>
2025-07-06 10:39:04 -0700

    Update BuildAndPreviewPage.tsx

--- frontend/src/pages/BuildAndPreviewPage.tsx
+++ frontend/src/pages/BuildAndPreviewPage.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import apiClient from '../services/api';
 import AppSimulator from '../components/core/AppSimulator';
