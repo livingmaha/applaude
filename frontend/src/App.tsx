41fdd2579538fdd4b19f273f213a5ed3df7ea629
John Mugambi Ndeke <mugambindeke@gmail.com>
2025-07-06 04:18:37 -0700

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { AuthProvider } from './contexts/AuthContext';
 
 import SignUpPage from './pages/SignUpPage';
@@ -10,8 +10,9 @@ import ProjectDetailPage from './pages/ProjectDetailPage';
 import ProtectedRoute from './components/core/ProtectedRoute';
 import AboutPage from './pages/AboutPage';
 import LandingPage from './pages/LandingPage';
-import ProfilePage from './pages/ProfilePage'; // Import the new page
+import ProfilePage from './pages/ProfilePage';
 import BuildAndPreviewPage from './pages/BuildAndPreviewPage';
+import UpgradeSubscriptionPage from './pages/UpgradeSubscriptionPage'; // Import the new page
 
 function App() {
   return (
@@ -27,10 +28,11 @@ function App() {
           {/* Protected Routes */}
           <Route element={<ProtectedRoute />}>
             <Route path="/dashboard" element={<Dashboard />} />
-            <Route path="/profile" element={<ProfilePage />} /> {/* Add profile route */}
+            <Route path="/profile" element={<ProfilePage />} />
             <Route path="/projects/create" element={<CreateProjectPage />} />
             <Route path="/projects/:id" element={<ProjectDetailPage />} />
             <Route path="/projects/:id/build" element={<BuildAndPreviewPage />} />
+            <Route path="/upgrade" element={<UpgradeSubscriptionPage />} /> {/* Add the new route */}
           </Route>
         </Routes>
       </Router>
