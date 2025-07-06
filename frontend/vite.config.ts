4a4632fd4270e4acbabd54bf7dbf929cc13ce2a0
John Mugambi Ndeke <mugambindeke@gmail.com>
2025-07-06 13:43:39 -0700

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,10 +5,4 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // Explicitly set the project root
-  root: __dirname,
-  build: {
-    // And the output directory, relative to the root
-    outDir: './dist',
-  },
 })
