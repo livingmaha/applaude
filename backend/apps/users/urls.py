eb46837eb7b11ebd76b8aad73eb4eb723c5b8986
John Mugambi Ndeke <mugambindeke@gmail.com>
2025-07-11 05:55:31 -0700

    Update urls.py

--- backend/apps/users/urls.py
+++ backend/apps/users/urls.py
@@ -1,4 +1,3 @@
-
 from django.urls import path
 from .views import CreateUserView, CreateTokenView, ManageUserView
 
@@ -7,5 +6,5 @@ app_name = 'users'
 urlpatterns = [
     path('create/', CreateUserView.as_view(), name='create'),
     path('token/', CreateTokenView.as_view(), name='token'),
-    path('me/', ManageUserView.as_view(), name='me'), # New endpoint
+    path('me/', ManageUserView.as_view(), name='me'),
 ]
