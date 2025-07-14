bda8e40a9f47aa4be2180368c896b4c99fd3525f
John Mugambi Ndeke <mugambindeke@gmail.com>
2025-07-14 02:23:27 -0700

    Create serializers.py

---
+++ backend/apps/blog/serializers.py
@@ -0,0 +1,14 @@
+from rest_framework import serializers
+from .models import BlogPost
+from apps.users.serializers import UserSerializer
+
+class BlogPostSerializer(serializers.ModelSerializer):
+    """
+    Serializer for the BlogPost model.
+    """
+    author = UserSerializer(read_only=True)
+
+    class Meta:
+        model = BlogPost
+        fields = ['id', 'title', 'content', 'main_image_url', 'author', 'is_published', 'created_at', 'updated_at']
+        read_only_fields = ['author', 'created_at', 'updated_at']
