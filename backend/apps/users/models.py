933a54b263c2df51b7dd3f363abec86f3dd5ef35
John Mugambi Ndeke <mugambindeke@gmail.com>
2025-07-15 18:10:06 +0300

    Update models.py

--- backend/apps/users/models.py
+++ backend/apps/users/models.py
@@ -3,6 +3,8 @@ from django.db import models
 
 class CustomUser(AbstractUser):
     is_premium_subscribed = models.BooleanField(default=False)
+    # **MODIFIED: Add field for GitHub OAuth token**
+    github_token = models.CharField(max_length=255, blank=True, null=True)
     
     def __str__(self):
         return self.username
