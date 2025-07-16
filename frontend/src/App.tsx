f259b9b9e60720965c761952acfd443a7e4d22b8
John Mugambi Ndeke <mugambindeke@gmail.com>
2025-07-16 15:45:16 +0300

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,49 +1,43 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import LandingPage from './pages/LandingPage';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { AuthProvider } from './contexts/AuthContext';
+import { Toaster } from 'sonner';
+
+import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
 import LoginPage from './pages/LoginPage';
 import SignupPage from './pages/SignupPage';
 import DashboardPage from './pages/DashboardPage';
-import CreateProjectPage from './pages/CreateProjectPage';
-import ProjectPreviewPage from './pages/ProjectPreviewPage';
 import ProjectDetailPage from './pages/ProjectDetailPage';
-import AboutPage from './pages/AboutPage';
 import FAQPage from './pages/FAQPage';
-import { AuthProvider, useAuth } from './contexts/AuthContext';
-import SuperuserRoute from './components/auth/SuperuserRoute';
-import ProjectAnalyticsPage from './pages/ProjectAnalyticsPage';
-import TermsOfServicePage from './pages/TermsOfServicePage';
-import PrivacyPolicyPage from './pages/PrivacyPolicyPage';
+import BlogPage from './pages/BlogPage';
+import BlogPostPage from './pages/BlogPostPage';
+import AdminBlogPage from './pages/AdminBlogPage';
+import CreateBlogPost from './pages/CreateBlogPost';
+import EditBlogPost from './pages/EditBlogPost';
 
-const PrivateRoute = ({ children }: { children: JSX.Element }) => {
-  const auth = useAuth();
-  return auth.isAuthenticated ? children : <Navigate to="/login" />;
-};
+import PrivateRoute from './components/auth/PrivateRoute';
 
 function App() {
   return (
     <AuthProvider>
       <Router>
+        <Toaster position="top-right" richColors />
         <Routes>
-          {/* Public Routes */}
-          <Route path="/" element={<LandingPage />} />
+          <Route path="/" element={<HomePage />} />
           <Route path="/about" element={<AboutPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup"element={<SignupPage />} />
           <Route path="/faq" element={<FAQPage />} />
           <Route path="/blog" element={<BlogPage />} />
-          <Route path="/blog/:id" element={<BlogPostPage />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/signup" element={<SignupPage />} />
-          <Route path="/terms" element={<TermsOfServicePage />} />
-          <Route path="/privacy" element={<PrivacyPolicyPage />} />
+          <Route path="/blog/:slug" element={<BlogPostPage />} />
 
-          {/* Private Routes */}
+          {/* Protected Routes */}
           <Route path="/dashboard" element={<PrivateRoute><DashboardPage /></PrivateRoute>} />
-          <Route path="/create-project" element={<PrivateRoute><CreateProjectPage /></PrivateRoute>} />
-          <Route path="/projects/:id/preview" element={<PrivateRoute><ProjectPreviewPage /></PrivateRoute>} />
-          <Route path="/projects/:id" element={<PrivateRoute><ProjectDetailPage /></PrivateRoute>} />
-          <Route path="/projects/:id/analytics" element={<PrivateRoute><ProjectAnalyticsPage /></PrivateRoute>} />
+          <Route path="/project/:id" element={<PrivateRoute><ProjectDetailPage /></PrivateRoute>} />
+          <Route path="/admin/blog" element={<PrivateRoute adminOnly={true}><AdminBlogPage /></PrivateRoute>} />
+          <Route path="/admin/blog/create" element={<PrivateRoute adminOnly={true}><CreateBlogPost /></PrivateRoute>} />
+          <Route path="/admin/blog/edit/:slug" element={<PrivateRoute adminOnly={true}><EditBlogPost /></PrivateRoute>} />
 
-          {/* Superuser Routes */}
-          <Route path="/admin/blog" element={<SuperuserRoute><BlogDashboard /></SuperuserRoute>} />
         </Routes>
       </Router>
     </AuthProvider>
