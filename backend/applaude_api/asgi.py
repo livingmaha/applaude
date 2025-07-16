14df7c56067345f4c7d0e7194929fd9759b72f9d
John Mugambi Ndeke <mugambindeke@gmail.com>
2025-07-16 19:47:09 +0300

    Update asgi.py

--- backend/applaude_api/asgi.py
+++ backend/applaude_api/asgi.py
@@ -2,15 +2,15 @@ import os
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-import apps.chat.routing
+import apps.api.routing
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'applause_api.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'applaude_api.settings')
 
 application = ProtocolTypeRouter({
   "http": get_asgi_application(),
   "websocket": AuthMiddlewareStack(
         URLRouter(
-            apps.chat.routing.websocket_urlpatterns
+            apps.api.routing.websocket_urlpatterns
         )
     ),
 })
