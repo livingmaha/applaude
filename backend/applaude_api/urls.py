e143d1c4d8f985ce002b30da7ddd3362281fca76
John Mugambi Ndeke <mugambindeke@gmail.com>
2025-07-16 19:35:36 +0300

    Update urls.py

--- backend/applaude_api/urls.py
+++ backend/applaude_api/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include('apps.api.urls')),
     path('api/users/', include('apps.users.urls')),
     path('api/projects/', include('apps.projects.urls')),
     path('api/payments/', include('apps.payments.urls')),
@@ -11,4 +13,7 @@ urlpatterns = [
     path('api/v1/blog/', include('apps.blog.urls')),
     path('api/analytics/', include('apps.analytics.urls')),
     path('api/testimonials/', include('apps.testimonials.urls')),
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 ]
