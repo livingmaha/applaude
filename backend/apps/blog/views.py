395f3f30258c0816f2fea7e83b958169eedf1291
John Mugambi Ndeke <mugambindeke@gmail.com>
2025-07-16 19:17:56 +0300

    Update views.py

--- backend/apps/blog/views.py
+++ backend/apps/blog/views.py
@@ -7,20 +7,11 @@ class IsSuperUserOrReadOnly(permissions.BasePermission):
     Custom permission to only allow superusers to edit objects.
     """
     def has_permission(self, request, view):
-        # Read permissions are allowed to any request,
-        # so we'll always allow GET, HEAD or OPTIONS requests.
         if request.method in permissions.SAFE_METHODS:
             return True
-        
-        # Write permissions are only allowed to superusers.
         return request.user and request.user.is_superuser
 
 class BlogPostListCreateView(generics.ListCreateAPIView):
-    """
-    API view to retrieve a list of blog posts or create a new one.
-    Superusers can create and see all posts.
-    Regular users can only see published posts.
-    """
     serializer_class = BlogPostSerializer
     permission_classes = [IsSuperUserOrReadOnly]
 
@@ -28,15 +19,12 @@ class BlogPostListCreateView(generics.ListCreateAPIView):
         if self.request.user.is_authenticated and self.request.user.is_superuser:
             return BlogPost.objects.all()
         return BlogPost.objects.filter(is_published=True)
-    
+
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
 
 
 class BlogPostRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
-    """
-    API view to retrieve, update, or delete a blog post.
-    """
     queryset = BlogPost.objects.all()
     serializer_class = BlogPostSerializer
     permission_classes = [IsSuperUserOrReadOnly]
