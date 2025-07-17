20cf8e2907f647a70d67e4e9c44604a54090cee2
John Mugambi Ndeke <mugambindeke@gmail.com>
2025-07-17 16:54:20 +0300

    Update production.py

--- backend/applaude_api/settings/production.py
+++ backend/applaude_api/settings/production.py
@@ -10,6 +10,19 @@ DEBUG = False
 
 ALLOWED_HOSTS = [".vercel.app", ".amazonaws.com"]
 
+# Security Headers
+SECURE_BROWSER_XSS_FILTER = True
+SECURE_CONTENT_TYPE_NOSNIFF = True
+X_FRAME_OPTIONS = 'DENY'
+SECURE_HSTS_SECONDS = 31536000 # 1 year
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_HSTS_PRELOAD = True
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+
+
 # AWS Secrets Manager
 def get_secret():
     secret_name = "applaude/production"
