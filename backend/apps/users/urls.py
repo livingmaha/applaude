8ed78c22b2fc2e3d75b6ec301464768eade113e6
John Mugambi Ndeke <mugambindeke@gmail.com>
2025-07-17 14:25:52 +0300

    Update urls.py

--- backend/apps/users/urls.py
+++ backend/apps/users/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import CreateUserView, CreateTokenView, ManageUserView
+from .views import CreateUserView, CreateTokenView, ManageUserView, export_user_data, delete_user_data 
 
 app_name = 'users'
 
@@ -7,4 +7,6 @@ urlpatterns = [
     path('create/', CreateUserView.as_view(), name='create'),
     path('token/', CreateTokenView.as_view(), name='token'),
     path('me/', ManageUserView.as_view(), name='me'),
+    path('export-data/', export_user_data, name='export-user-data'),
+    path('delete-account/', delete_user_data, name='delete-user-data'),
 ]
