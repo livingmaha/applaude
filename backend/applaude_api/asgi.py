9c15a12c9f40afa98010f182eb5c4d3dbdec0324
John Mugambi Ndeke <mugambindeke@gmail.com>
2025-07-17 15:08:42 +0300

    Update asgi.py

--- backend/applaude_api/asgi.py
+++ backend/applaude_api/asgi.py
@@ -1,14 +1,22 @@
 import os
-
-from channels.auth import AuthMiddlewareStack
+import django
 from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
 from django.core.asgi import get_asgi_application
 import apps.projects.routing
 
+# Set the default settings module for the 'asgi' program.
+# It's crucial this points to your production settings in the deployed environment.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'applaude_api.settings.production')
 
+# Initialize Django application
+django.setup()
+
+# The default ASGI application for standard HTTP requests
+django_asgi_app = get_asgi_application()
+
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
+    "http": django_asgi_app,
     "websocket": AuthMiddlewareStack(
         URLRouter(
             apps.projects.routing.websocket_urlpatterns
