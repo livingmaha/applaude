e289eecba479cbf629c7bbf2ae3c7795b64ebe50
John Mugambi Ndeke <mugambindeke@gmail.com>
2025-07-17 14:35:26 +0300

    Update production.py

--- backend/applaude_api/settings/production.py
+++ backend/applaude_api/settings/production.py
@@ -1,10 +1,26 @@
 from .base import *
 import boto3
 import json
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
+from sentry_sdk.integrations.celery import CeleryIntegration
+from sentry_sdk.integrations.redis import RedisIntegration
 
 DEBUG = False
 
 ALLOWED_HOSTS = [".vercel.app", ".amazonaws.com"]
+
+sentry_sdk.init(
+    dsn=secrets['SENTRY_DSN'],
+    integrations=[
+        DjangoIntegration(),
+        CeleryIntegration(),
+        RedisIntegration(),
+    ],
+    traces_sample_rate=1.0,
+    send_default_pii=True,
+    environment="production"
+)
 
 # AWS Secrets Manager
 def get_secret():
@@ -56,5 +72,19 @@ CHANNEL_LAYERS = {
         "CONFIG": {
             "hosts": [(secrets['REDIS_HOST'], secrets['REDIS_PORT'])],
         },
+    },
+}
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+        },
+    },
+    "root": {
+        "handlers": ["console"],
+        "level": "INFO",
     },
 }
