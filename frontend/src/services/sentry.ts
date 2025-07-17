8031bb53f510a91b20053f8038b5cb032c3431d2
John Mugambi Ndeke <mugambindeke@gmail.com>
2025-07-17 14:33:23 +0300

    Create sentry.ts

---
+++ frontend/src/services/sentry.ts
@@ -0,0 +1,13 @@
+import * as Sentry from "@sentry/react";
+import { BrowserTracing } from "@sentry/tracing";
+
+export function initSentry() {
+  if (import.meta.env.PROD) {
+    Sentry.init({
+      dsn: import.meta.env.VITE_SENTRY_DSN,
+      integrations: [new BrowserTracing()],
+      tracesSampleRate: 1.0,
+      environment: import.meta.env.MODE,
+    });
+  }
+}
