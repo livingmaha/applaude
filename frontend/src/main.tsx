c9cac4e3177b3cec442a525f7ee7ec775a389947
John Mugambi Ndeke <mugambindeke@gmail.com>
2025-07-19 11:07:45 +0300

    Update main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
+import { useAuthStore } from './stores/useAuth.ts';
+
+// Initialize auth state on application load
+useAuthStore.getState().initializeAuth();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
